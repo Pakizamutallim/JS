c707cff465af40fe2733b571b3926493bc5b7f97
Pakizamutallim <127186291+Pakizamutallim@users.noreply.github.com>
2023-04-27 09:07:28 +0400

    Add files via upload

---
+++ ev tapsirigi 27.04/assets/js/main.js
@@ -0,0 +1,9 @@
+    var check = false;
+    var say = 0;
+    var myarray = [eli, veli, pirveli, hesen, gelin, nefer]; 
+    myarray.includes("eli");
+    if (check = true) {
+        say++;
+        console.log(); 
+    }
+    
